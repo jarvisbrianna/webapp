7cdb8e2e70f85620f6a9c732989a76fd2df26484
Brianna Jarvis <briannajarvis@Briannas-MacBook-Air-51.local>
2025-12-02 18:22:37 -0500

    hi

--- webappdevelopment.R
+++ webappdevelopment.R
@@ -28,3 +28,17 @@ file.edit(".gitignore") # make new file of things for git to ignore
 # check files
 check_site()
 y
+
+# pushed to github and uploaded to netlify
+
+install.packages("rstudioapi")
+library(rstudioapi) # to easily navigate to files
+rstudioapi::navigateToFile("config.toml") # to be able to put new URL in
+
+check_site() # again
+
+rstudioapi::navigateToFile("netlify.toml") 
+
+# after this, given everything went correctly (which it might not have but I guess I'll never
+# know) I would be able to have a real website using this code (but I am broke and do not want
+# to pay for a domain) 
